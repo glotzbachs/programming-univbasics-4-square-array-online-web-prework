ec874a613c7b9f04c11655d4335ba8e315047794
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-09-17 19:06:44 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,9 @@
 def square_array(array)
   counter=0
   while array[counter] do
-    new_array = []
-    array.each do |array[counter]|
-        new_array << array[counter] ** 2
-      counter+=1
+    squares = []
+    squares << array[counter] ** 2
+    counter+=1
     end
     new_array
 end
